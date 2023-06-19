c432c08f1d6c6809e75049a5ba37085d4d7e2421
Hieu Phan <phanhieu.hust64@gmail.com>
2023-06-19 22:36:59 +0700

    Edit Main.java

---
+++ src/main/java/DBconnect.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnect {
+    private final String  _url = "jdbc:mysql://127.0.0.1:3306/mydb";
+    private final String _userName = "root";
+    private final String _password = "phanhieu08";
+    static Connection connection;
+    public DBconnect() throws SQLException {
+        connection = DriverManager.getConnection(_url, _userName, _password);
+    }
+
+}
